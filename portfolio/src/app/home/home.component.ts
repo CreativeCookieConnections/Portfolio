03ad55131a7abaeed4d89be5cb51ab25932222e6
Aisha Keller <creativecookieconnections@gmail.com>
2026-05-23 22:25:33 -0500

    applied css to recolorize the social media container in the home component. Wanted to apply a style that allowed that part of the component to stand out.

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -77,7 +77,7 @@ textPath {
   text-align: center;
   margin-top: -4rem;
   padding: 2rem;
-  background-color: #d1bbbb;
+  background-color: #7a6c7a;
   border-radius: 100px;
   border: 2px solid #b89090;
   box-shadow: 2px 2px 8px #22180b;
